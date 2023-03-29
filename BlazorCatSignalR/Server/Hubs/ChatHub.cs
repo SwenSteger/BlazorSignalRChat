3eee4535f3ce78ae4f50d42da3641ff54e56a31e
nGAGEOnline <ngage@ziggo.nl>
2023-03-29 11:24:54 +0200

    Initial Project Commit

---
+++ BlazorCatSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorCatSignalR.Server.Hubs;
+
+public class ChatHub : Hub
+{
+	private static Dictionary<string, string> _users = new();
+	
+	public override async Task OnConnectedAsync()
+	{
+		var username = Context.GetHttpContext().Request.Query["username"];
+		_users.Add(Context.ConnectionId, username);
+		await SendMessage(string.Empty, $"{username} Connected!");
+		await base.OnConnectedAsync();
+	}
+
+	public override async Task OnDisconnectedAsync(Exception? exception)
+	{
+		var username = _users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+		await SendMessage(string.Empty, $"{username} Disconnected!");
+		await base.OnDisconnectedAsync(exception);
+	}
+	
+	public async Task SendMessage(string user, string message)
+	{
+		await Clients.All.SendAsync("ReceiveMessage", user, message);
+	}
+}
